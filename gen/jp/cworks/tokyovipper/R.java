46bbcd3fd19325c94560b9bb861009f3ceb06479
m-satoh <satoh@manasoft.co.jp>
2014-06-25 21:18:53 +0900

    commit

--- gen/jp/cworks/tokyovipper/R.java
+++ gen/jp/cworks/tokyovipper/R.java
@@ -15,11 +15,6 @@ public final class R {
         public static final int abc_slide_in_top=0x7f040003;
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
-        public static final int alpha=0x7f040006;
-        public static final int alpha_close_enter=0x7f040007;
-        public static final int alpha_close_exit=0x7f040008;
-        public static final int alpha_open_enter=0x7f040009;
-        public static final int alpha_open_exit=0x7f04000a;
     }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
@@ -1102,13 +1097,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003e;
+        public static final int container=0x7f050041;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1117,18 +1112,20 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int gallery1=0x7f050040;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int imageView1=0x7f05003f;
-        public static final int imageView2=0x7f050040;
-        public static final int imageView3=0x7f050041;
-        public static final int imageView4=0x7f050042;
+        public static final int imageView2=0x7f050042;
+        public static final int imageView3=0x7f050043;
+        public static final int imageView4=0x7f050044;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int logo=0x7f05003c;
+        public static final int logobtn=0x7f05003e;
         public static final int logomoji=0x7f05003d;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1211,8 +1208,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_logo=0x7f030018;
-        public static final int activity_start=0x7f030019;
-        public static final int fragment_start=0x7f03001a;
+        public static final int activity_select=0x7f030019;
+        public static final int activity_start=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
@@ -1264,20 +1261,7 @@ containing a value of this type.
         public static final int hello_world=0x7f0a000e;
     }
     public static final class style {
-        /**  Application theme. 
-         */
-        public static final int Alpha_Effect=0x7f0b008d;
-        public static final int AlphaEffect=0x7f0b008e;
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -1290,9 +1274,6 @@ containing a value of this type.
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f0b008b;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
         public static final int AppTheme=0x7f0b008c;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
--- src/jp/cworks/tokyovipper/LogoActivity.java
+++ src/jp/cworks/tokyovipper/LogoActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LogoActivity extends Activity {
@@ -16,26 +18,41 @@ public class LogoActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_logo);
 
-		ImageView logo = (ImageView) findViewById(R.id.logo);
-		Animation logoanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
-		logo.startAnimation(logoanim);
+//		ImageView logo = (ImageView) findViewById(R.id.logo);
+//		Animation logoanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+//		logo.startAnimation(logoanim);
 
-		ImageView logomoji = (ImageView) findViewById(R.id.logomoji);
-		Animation logoanimmoji = AnimationUtils.loadAnimation(this,
-				R.anim.alpha);
-		logomoji.startAnimation(logoanimmoji);
+//		ImageView logomoji = (ImageView) findViewById(R.id.logomoji);
+//		Animation logoanimmoji = AnimationUtils.loadAnimation(this,
+//				R.anim.alpha);
+//		logomoji.startAnimation(logoanimmoji);
+		Button b = (Button) findViewById(R.id.logobtn);
+        b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), StartActivity.class);
+				startActivity(i);
+				finish();
+			}
+		});
 	}
-
+/*
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		switch (event.getAction()) {
 		default:
-			Intent intent = new Intent(this, StartActivity.class);
-			startActivity(intent);
-			finish();
+			runOnUiThread(new Runnable(){
+		        @Override
+		        public void run() {
+					Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+					startActivity(intent);
+		        }
+		    });
 			break;
 		}
+//		return super.onTouchEvent(event);
 		return super.onTouchEvent(event);
 	}
-
+*/
 }
